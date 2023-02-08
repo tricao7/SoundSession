581de46ad8a933fd31cb9b54c48ff0dd95260a50
Corey Miller <cnmill01@gmail.com>
2023-02-08 14:43:47 -0700

    Testing note

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Note from Corey: If you're reading this, our site deploys fine!
         </p>
         <a
           className="App-link"
